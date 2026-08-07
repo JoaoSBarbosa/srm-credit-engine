9b2eb5a0f79d77a2de0d687665291fc27a3b2f3b
João Barbosa <contato.jsbarbosa@gmail.com>
2026-08-07 00:18:56 -0300

    feat(database): add initial seed data for currency, assignor and exchange rates

---
+++ backend/src/main/resources/db/migration/V10__seed_assignor.sql
@@ -0,0 +1,3 @@
+INSERT INTO assignor
+    (id, name, document, created_at, updated_at)
+VALUES ('f8f65109-71fd-42e0-bfda-fa856e5b32da', 'SRM Capital LTDA', '12345678000199', NOW(), NOW());
---
+++ backend/src/main/resources/db/migration/V9__seed_currency.sql
@@ -0,0 +1,16 @@
+INSERT INTO currency
+(id,
+ iso_code,
+ name,
+ created_at,
+ updated_at)
+VALUES ('f4250be8-8de8-47a2-8f3b-4c50a075eb38', 'BRL', 'Real Brasileiro', NOW(), NOW());
+
+
+INSERT INTO currency
+(id,
+ iso_code,
+ name,
+ created_at,
+ updated_at)
+VALUES ('ec2180f0-fe7f-4349-823e-fed69310c470', 'USD', 'Dolar Americano', NOW(), NOW());
