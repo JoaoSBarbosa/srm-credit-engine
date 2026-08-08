78139f329c7c961d053b5e4282c8c74d55fba6f5
João Barbosa <contato.jsbarbosa@gmail.com>
2026-08-08 01:58:33 -0300

    refactor: reorganize settlement repository

--- backend/src/main/java/br/com/joaobarbosa/srm/creditengine/repository/SettlementRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.joaobarbosa.srm.creditengine.repository;
-
-import br.com.joaobarbosa.srm.creditengine.model.entity.Settlement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SettlementRepository extends JpaRepository<Settlement, UUID> {}
